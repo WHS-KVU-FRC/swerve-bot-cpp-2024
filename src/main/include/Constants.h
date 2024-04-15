b1e302f2de96bc527f86f3809adfaca478170990
kylenvu <kylenvu@gmail.com>
2024-04-14 20:43:30 -0700

    added drivebase constants

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -14,7 +14,6 @@
 using namespace units::literals;
 
 namespace SwerveModuleConstants {
-
 static units::volt_t constexpr kSDrive = 1_V; // kS feedforward
 static auto constexpr kVDrive = 3_V / 1_mps;  // kV feedforward
 static double constexpr kPDrive = 1.0;        // kP PID feedback
@@ -34,7 +33,13 @@ static units::radians_per_second_t constexpr kMaxAngularVelocity =
   std::numbers::pi * 1_rad_per_s;
 static units::radians_per_second_squared_t constexpr kMaxAngularAcceleration =
   std::numbers::pi * 2_rad_per_s_sq;
-
 } // SwerveModuleConstants
+
+namespace DrivebaseConstants {
+static units::meter_t constexpr kWheelbase = 0.3_m;   // length from top to bottom
+static units::meter_t constexpr kTrackwidth = 0.3_m;  // width from left to right
+static units::meters_per_second_t constexpr kMaxSpeed = 3.0_mps;
+static units::radians_per_second_t constexpr kMaxAngularSpeed = units::radians_per_second_t{std::numbers::pi};
+} // DrivebaseConstants
 
 #endif  // #ifndef _CONSTANTS_H_
